84f288227858bc324448742e76d0f0b2d85eccff
odia221 <odia221@localhost.localdomain>
2023-06-22 05:17:03 +0000

    documenter services

--- src/components/Service/OurServices.js
+++ src/components/Service/OurServices.js
@@ -6,15 +6,79 @@ const cardsData = [
   {
     id: 1,
     title: 'Formations et certifications sur le numérique',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet semper nibh, in gravida elit. Sed ut ipsum vitae urna efficitur aliquam id in lacus. Sed sed gravida est. Fusce pharetra, velit at sagittis fermentum, dui neque aliquam justo, a porta tortor sapien vitae risus. Fusce faucibus, sapien eget convallis ullamcorper, tortor dui feugiat ipsum, nec congue lorem mi eget nisi. Vestibulum et efficitur elit, at eleifend dui. Ut et convallis ante, vitae feugiat sapien. Curabitur ultrices vulputate justo. Quisque bibendum venenatis nisl, sit amet scelerisque nisl imperdiet in. Mauris vestibulum sem id eleifend semper. Fusce sollicitudin elementum arcu, at viverra eros consequat non. Nam id neque enim. Aliquam tristique turpis a ante efficitur fermentum. Mauris eu magna ac tellus ullamcorper fringilla at id justo.',
+    content:`
+    Chez Wommate, nous sommes dédiés à fournir des formations de haute qualité et des certifications reconnues dans le domaine du numérique. Notre objectif est de vous aider à développer les compétences essentielles et à acquérir une expertise pointue dans les métiers du numérique, afin de rester compétitif dans un monde en constante évolution.
+    
+    Nos formations sont soigneusement conçues pour répondre aux besoins spécifiques de l'industrie et sont dispensées par des experts chevronnés et passionnés par leur domaine d'expertise. Que vous soyez un professionnel en reconversion, un entrepreneur cherchant à développer ses compétences numériques ou un étudiant désireux de se préparer pour une carrière prometteuse, nos programmes de formation sont conçus pour vous aider à atteindre vos objectifs.
+    
+    En nous choisissant, vous bénéficiez d'une expérience d'apprentissage immersive et engageante, grâce à notre approche pédagogique novatrice. Nos équipes combinent des cours théoriques approfondis avec des exercices pratiques, des études de cas réels et des projets concrets, afin de renforcer vos connaissances et de vous donner les compétences nécessaires pour réussir dans le monde professionnel.
+    
+    Nous offrons une vaste gamme de formations couvrant des domaines clés du numérique tels que :
+    
+    - Le développement web
+    - La programmation
+    - Le marketing digital
+    - L'analyse de données
+    - La cybersécurité
+    - Les objets connectés
+    - La communication
+    - L'entrepreneuriat
+    
+    Que vous soyez intéressé par le développement de sites web interactifs, la création de campagnes de marketing numérique percutantes ou la protection des données contre les cyberattaques, nous avons le programme de formation qui correspond à vos besoins et à vos aspirations.
+    
+    En plus de nos formations, nous proposons des certifications qui vous permettent de valider vos compétences et de vous démarquer sur le marché du travail. Nos certifications peuvent ouvrir de nouvelles opportunités professionnelles passionnantes.
+    
+    Rejoignez-nous dès aujourd'hui et transformez votre avenir grâce à nos formations et certifications numériques de premier ordre. Donnez un coup d'accélérateur à votre carrière et devenez un expert recherché dans le monde du numérique. Faites le choix de la perfection.
+    
+    Contactez-nous dès maintenant pour en savoir plus sur nos programmes de formation et nos certifications, et commencer votre voyage vers le succès numérique dès aujourd'hui !
+    
+    Wommate TECH, Votre partenaire de confiance pour la réussite dans le monde numérique.
+        `,
+    
   },
   {
     id: 2,
     title: 'Accompagnement des PMEs dans la digitalisation',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet semper nibh, in gravida elit. Sed ut ipsum vitae urna efficitur aliquam id in lacus. Sed sed gravida est. Fusce pharetra, velit at sagittis fermentum, dui neque aliquam justo, a porta tortor sapien vitae risus. Fusce faucibus, sapien eget convallis ullamcorper, tortor dui feugiat ipsum, nec congue lorem mi eget nisi. Vestibulum et efficitur elit, at eleifend dui. Ut et convallis ante, vitae feugiat sapien. Curabitur ultrices vulputate justo. Quisque bibendum venenatis nisl, sit amet scelerisque nisl imperdiet in. Mauris vestibulum sem id eleifend semper. Fusce sollicitudin elementum arcu, at viverra eros consequat non. Nam id neque enim. Aliquam tristique turpis a ante efficitur fermentum. Mauris eu magna ac tellus ullamcorper fringilla at id justo.',
-  },
+    content:`
+    Bienvenue dans notre entreprise spécialisée dans l'accompagnement des PME dans leur processus de digitalisation !
+
+Nous comprenons les défis auxquels les PME sont confrontées lorsqu'il s'agit de se digitaliser et d'exploiter les opportunités offertes par le monde numérique. C'est pourquoi nous proposons un accompagnement personnalisé pour aider les entreprises à insérer la digitalisation au cœur de leur fonctionnement et de leurs services, en vue de stimuler leur croissance et leur compétitivité.
+
+Notre approche est centrée sur vos besoins spécifiques. Nous travaillons en étroite collaboration avec vous pour comprendre vos objectifs, votre secteur d'activité et votre culture d'entreprise. En utilisant notre expertise approfondie dans les technologies numériques et les meilleures pratiques du secteur, nous concevons des solutions sur mesure qui vous permettent de tirer parti de tout le potentiel de la transformation numérique.
+
+Nos services d'accompagnement ont fourni un large éventail de domaines, tels que :
+
+1- Diagnostic et planification stratégique : 
+Nous évaluons votre situation actuelle et identifions les opportunités de digitalisation spécifiques à votre entreprise. Ensuite, nous élaborons un plan stratégique détaillé qui vous guidera tout au long du processus de transformation digitale.
+
+2- Mise en place d'une infrastructure numérique solide : 
+Nous vous aidons à choisir les outils et les technologies adaptées à vos besoins, tels que les systèmes de gestion, les plateformes de commerce électronique, les solutions de CRM (Customer Relationship Management), les outils d'automatisation des processus, etc.
+
+3- Formation et sensibilisation : 
+Nous proposons des programmes de formation adaptés aux besoins de vos équipes, pour les aider à acquérir les compétences numériques nécessaires à l'utilisation efficace des nouvelles technologies et des processus numériques.
+
+3- Transformation des processus métier : 
+Nous analysons vos processus existants et vous aidons à optimiser en intégrant des solutions numériques pour renforcer l'efficacité, réduire les coûts et améliorer la qualité des services.
+
+3- Développement d'une présence en ligne et marketing digital : 
+Nous vous accompagnons dans la création d'une présence en ligne professionnelle, en développant des sites web conviviaux, en mettant en place des stratégies de marketing digital personnalisées et en vous aidant à utiliser les réseaux sociaux pour augmenter votre visibilité et attirer de nouveaux clients.
+
+4- Sécurité et protection des données : 
+Nous veillons à ce que votre entreprise adopte des mesures de sécurité conformes pour protéger vos données sensibles, conformément aux réglementations en vigueur, tout en garantissant une expérience utilisateur fluide et sécurisée.
+
+Notre objectif est de faire de votre transition digitale une expérience réussie, en vous guidant à chaque étape du processus. Nous sommes là pour vous aider à naviguer dans l'univers numérique en évolution constante et à exploiter tout le potentiel offert par la digitalisation.
+
+Contactez-nous dès aujourd'hui pour en savoir plus sur nos services d'accompagnement en digitalisation des PME et essayez de transformer votre entreprise pour une croissance durable et une compétitivité renforcée !
+
+
+WOMMATE TECH !!!
+
+
+
+
+
+
+    `  },
   {
     id: 3,
     title: 'Services de développement logiciel',
@@ -41,7 +105,7 @@ const OurServices = () => {
   };
 
   return (
-    <div>
+    <div id='bodyService'>
       <h1 className="titleServices">Nos Services</h1>
       <div className="containerServices">
         {cardsData.map((card) => (
