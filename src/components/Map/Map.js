020dc9b00b7628bbe18f15ef8f887703012f749a
odia221 <odia221@localhost.localdomain>
2023-06-27 03:12:06 +0000

    api ok

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,32 +1,25 @@
-import React, { useEffect } from 'react';
-import L from 'leaflet';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import './Map.css';
 
 const Map = () => {
-  useEffect(() => {
-    const latitude = 14.7167; // Latitude du Sénégal
-    const longitude = -17.4677; // Longitude du Sénégal
-    const zoom = 6;
+  const position = [14.6884, -17.4569]; // Coordonnées du siège de Simplon Sénégal
 
-    // Création de la carte
-    const map = L.map('map').setView([latitude, longitude], zoom);
-
-    // Ajout du fond de carte
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data © OpenStreetMap contributors',
-      maxZoom: 18,
-    }).addTo(map);
-
-    // Ajout d'un marqueur pour l'emplacement du Sénégal
-    L.marker([latitude, longitude]).addTo(map);
-
-    // Nettoyage de la carte lorsque le composant est démonté
-    return () => {
-      map.remove();
-    };
-  }, []);
-
-  return <div id="map" />;
+  return (
+    <MapContainer center={position} zoom={16} scrollWheelZoom={false} className="map-container">
+      <TileLayer
+        attribution='&copy; <a href="#">Wommate TECH</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+        Siège  Wommate TECH. <br />
+        Thoés, Sénégal rue 10
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 };
 
 export default Map;
